14d63c81387cf78dbae5bec523406c833bbc8dec
qsaifudin <duafinis@gmail.com>
2023-11-13 11:42:18 +0700

    baseurl

--- radiodiagnostic system/FE-Radiodiagnostic/src/routes/Config.js
+++ radiodiagnostic system/FE-Radiodiagnostic/src/routes/Config.js
@@ -1,3 +1,4 @@
-export const baseURL = "http://0.0.0.0:5000"
+// export const baseURL = "http://localhost:5000"
+export const baseURL = "http://178.16.142.41:5000"
 
 // export const baseURL = "http://api.radiodiagnosis-report.me"
