aded916e195f9e06bc960b0ba02089b381a3234b
itjavamifi <it.javamifi@gmail.com>
2024-01-14 20:44:15 +0700

    fe

--- radiodiagnostic system/FE-Radiodiagnostic/src/routes/Config.js
+++ radiodiagnostic system/FE-Radiodiagnostic/src/routes/Config.js
@@ -1,4 +1,4 @@
 // export const baseURL = "http://localhost:5000"
-export const baseURL = "http://178.16.142.41:5000"
+export const baseURL = "http://178.16.142.41:1001"
 
 // export const baseURL = "http://api.radiodiagnosis-report.me"
